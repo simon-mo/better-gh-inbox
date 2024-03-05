6f8839cfe6c772e909d17247089dd12b8914b5b6
simon-mo <simon.mo@hey.com>
2024-03-04 22:56:08 -0800

    initial prototype w/ command keys

---
+++ src/app/thread/[id]/page.tsx
@@ -0,0 +1,227 @@
+"use client";
+
+import {
+  getComment,
+  getComments,
+  getPullRequest,
+  getThread,
+} from "@/utils/get-data";
+import { get } from "http";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { Badge } from "@/components/ui/badge";
+import { remark } from "remark";
+import html from "remark-html";
+import Link from "next/link";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+
+function Issue({ data }) {
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <Badge>{data.reason}</Badge>
+    </div>
+  );
+}
+const markdownRenderer = remark().use(html);
+
+function PullRequest({ data }) {
+  const [pr, setPR] = useState(null);
+  const [latestComment, setLatestComment] = useState(null);
+  const [bodyHTML, setBodyHTML] = useState(null);
+  const [comments, setComments] = useState(null);
+
+  const [openCommand, setOpenCommand] = useState(false); // move this to global layout
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      // cmd + k
+      if (event.metaKey && event.key === "k") {
+        event.preventDefault();
+        setOpenCommand(true);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const prID = data.url.split("/").pop();
+      const result = await getPullRequest(prID);
+      setPR(result);
+
+      if (data.lastest_comment_url) {
+        const comment = await getComment(
+          data.lastest_comment_url.split("/").pop()
+        );
+        setLatestComment(comment);
+      }
+
+      const comments = await getComments(prID);
+      setComments(comments);
+
+      const markdownRendered = await markdownRenderer.process(result.body);
+
+      setBodyHTML(markdownRendered);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="m-8 mx-16">
+      {openCommand ? (
+        <CommandDialog open={openCommand} onOpenChange={setOpenCommand}>
+          <Command>
+            <CommandInput placeholder="Type a command or search..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Suggestions">
+                <CommandItem
+                  onSelect={() => {
+                    setOpenCommand(false);
+                    open(pr.url);
+                  }}
+                >
+                  Open Link
+                </CommandItem>
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </CommandDialog>
+      ) : null}
+
+      <h1 className="text-lg">{data.title}</h1>
+      <Badge>{data.reason}</Badge>
+
+      {pr && (
+        <div className="my-8">
+          State: <Badge>{pr.state}</Badge>
+          <br />
+          <Link href={pr.url}>View on GitHub</Link>
+        </div>
+      )}
+
+      {latestComment && (
+        <div className="my-8">
+          <h2>Latest Comment</h2>
+          <p>{latestComment.body}</p>
+        </div>
+      )}
+
+      <p>{JSON.stringify(data)}</p>
+      <p>{JSON.stringify(pr)}</p>
+
+      <div
+        className="markdown-body border-gray-600 shadow-md p-4 rounded-md"
+        dangerouslySetInnerHTML={{ __html: bodyHTML }}
+      ></div>
+
+      {comments && (
+        <div>
+          <h2>Comments</h2>
+          {comments.map((comment) => (
+            <div key={comment.id} className="my-4">
+              {/* render the avatar */}
+              <img
+                className="w-8 h-8 rounded-full"
+                src={comment.authorAvatarUrl}
+                alt={comment.author}
+              />
+
+              {/* render the author */}
+              <p className="inline ml-2">{comment.author}</p>
+
+              <div
+                className="markdown-body border-gray-600 shadow-md p-4 rounded-md"
+                dangerouslySetInnerHTML={{
+                  __html: markdownRenderer.processSync(comment.body),
+                }}
+              ></div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default function Page({ params }: { params: { id: string } }) {
+  const [data, setData] = useState(null);
+  const router = useRouter();
+
+  const [prev, setPrev] = useState(null);
+  const [next, setNext] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getThread(params.id);
+      setData(result);
+      const notifList = JSON.parse(
+        window.sessionStorage.getItem("notifications")
+      );
+      setPrev(notifList.find((notif) => notif.next_id === params.id)?.id);
+      setNext(notifList.find((notif) => notif.prev_id === params.id)?.id);
+    };
+
+    fetchData();
+  }, []);
+
+  // register keyboard listener for j and k
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // if cmd is pressed, don't do anything
+      if (event.metaKey) {
+        return;
+      }
+
+      if (event.key === "j") {
+        router.push(`/thread/${next}`);
+      } else if (event.key === "k") {
+        router.push(`/thread/${prev}`);
+      }
+
+      // on esc, push the user back to the main page
+      else if (event.key === "Escape") {
+        router.push("/");
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [data]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>
+        {data.type === "Issue" ? (
+          <PullRequest data={data} />
+        ) : data.type === "PullRequest" ? (
+          <PullRequest data={data} />
+        ) : (
+          <div>Unknown type: {data.type}</div>
+        )}
+      </h1>
+    </div>
+  );
+}
---
+++ src/utils/get-data.tsx
@@ -0,0 +1,120 @@
+"use server";
+
+import { Octokit } from "octokit";
+import { remark } from "remark";
+import html from "remark-html";
+
+function getDayString(daysAgo: number) {
+  const date = new Date();
+  date.setDate(date.getDate() - daysAgo);
+  return date.toISOString();
+}
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+async function getThread(id: number) {
+  const thread = await octokit.rest.activity.getThread({
+    thread_id: id,
+  });
+
+  return {
+    id: thread.data.id,
+    title: thread.data.subject.title,
+    url: thread.data.subject.url,
+    type: thread.data.subject.type,
+    reason: thread.data.reason,
+    lastest_comment_url: thread.data.subject.latest_comment_url,
+  };
+}
+
+const markdownRenderer = remark().use(html);
+
+async function getPullRequest(id: number) {
+  const pr = await octokit.rest.issues.get({
+    owner: "vllm-project",
+    repo: "vllm",
+    issue_number: id,
+  });
+
+  return {
+    id: pr.data.id,
+    title: pr.data.title,
+    body: pr.data.body,
+    state: pr.data.state,
+    url: pr.data.html_url,
+  };
+}
+
+async function getComments(id: number) {
+  const comments = await octokit.rest.issues.listComments({
+    owner: "vllm-project",
+    repo: "vllm",
+    issue_number: id,
+  });
+
+  return comments.data.map((comment) => {
+    return {
+      id: comment.id,
+      body: comment.body,
+      url: comment.html_url,
+      author: comment.user.login,
+      authorAvatarUrl: comment.user.avatar_url,
+    };
+  });
+}
+
+async function getComment(id: number) {
+  const comment = await octokit.rest.issues.getComment({
+    owner: "vllm-project",
+    repo: "vllm",
+    comment_id: id,
+  });
+
+  return {
+    id: comment.data.id,
+    body: comment.data.body,
+    url: comment.data.html_url,
+    author: comment.data.user.login,
+    authorAvatarUrl: comment.data.user.avatar_url,
+  };
+}
+
+async function getMyNotifications() {
+  const notifications = await octokit.paginate(
+    octokit.rest.activity.listRepoNotificationsForAuthenticatedUser,
+    {
+      owner: "vllm-project",
+      repo: "vllm",
+      all: true,
+      since: getDayString(7),
+      per_page: 100,
+    }
+  );
+
+  return notifications.map((notification, i) => {
+    return {
+      id: notification.id,
+      unread: notification.unread,
+      reason: notification.reason,
+      updated_at: notification.updated_at,
+      last_read_at: notification.last_read_at,
+      prev_id: i > 0 ? notifications[i - 1].id : null,
+      next_id: i < notifications.length - 1 ? notifications[i + 1].id : null,
+      subject: {
+        title: notification.subject.title,
+        url: notification.subject.url,
+        latest_comment_url: notification.subject.latest_comment_url,
+        type: notification.subject.type,
+      },
+    };
+  });
+}
+
+export {
+  getMyNotifications,
+  getThread,
+  getPullRequest,
+  getComment,
+  getComments,
+};
